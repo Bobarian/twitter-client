a358445ef51f74c1e4b47c2be2d4cbbb86f68d47
Bob Schoenthal <BSchoenthal@gmail.com>
2014-01-13 21:20:35 -0800

    Added Bootstrap to Gem, and made a tweet appear on page.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '3.2.13'
 
 gem 'sqlite3'
 
+# Bootstrap
+gem "twitter-bootstrap-rails", "~> 2.2.8"
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,7 +1,12 @@
 $.ajax({
   url: "/api/retrieveTweets/abcd",
   type: "GET",
-  success: function(response) {
-    debugger;
+  success: function(bob) {
+    insertData(bob)
   }
 })
+
+function insertData(JSONdata) {
+	debugger;
+	$(".tweets").html(JSONdata.statuses[4].text  +  " " + JSONdata.statuses[4].favorite_count);
+}
