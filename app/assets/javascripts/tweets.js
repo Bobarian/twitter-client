74379e656f2aeb6d32a3cd2e21ae139e1cd46a55
Bob Schoenthal <BSchoenthal@gmail.com>
2014-01-15 17:33:39 -0800

    Feeding twitter posts to page, plus a little styling.

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -8,5 +8,16 @@ $.ajax({
 
 function insertData(JSONdata) {
 	debugger;
+	for (var i = 0; i < JSONdata.statuses.length; i++) {
+		
+		$("#tweetList").append("<li class='list-group-item'><div class='Htitle'><h2 class='userName'>" + JSONdata.statuses[i].user.screen_name + "</h2><h4 class='UserLoc'>From: " + JSONdata.statuses[i].user.location + "</h4></div><p class='tweet'>" + JSONdata.statuses[i].text + "</p><h5 class='time'>" + JSONdata.statuses[i].created_at + "</h5>" + "</li>")
+
+
+
+
+
+
+	};
 	$(".tweets").html(JSONdata.statuses[4].text  +  " " + JSONdata.statuses[4].favorite_count);
-}
+	 
+} 
