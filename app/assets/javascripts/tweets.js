7ddd8af3e7dd592bf2bd11b7ea5f6b12a03ae4d1
Bob Schoenthal <BSchoenthal@gmail.com>
2014-01-16 22:06:56 -0800

    This is our test files for list.js

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -7,17 +7,15 @@ $.ajax({
 })
 
 function insertData(JSONdata) {
-	debugger;
+	
 	for (var i = 0; i < JSONdata.statuses.length; i++) {
 		
-		$("#tweetList").append("<li class='list-group-item'><div class='Htitle'><h2 class='userName'>" + JSONdata.statuses[i].user.screen_name + "</h2><h4 class='UserLoc'>From: " + JSONdata.statuses[i].user.location + "</h4></div><p class='tweet'>" + JSONdata.statuses[i].text + "</p><h5 class='time'>" + JSONdata.statuses[i].created_at + "</h5>" + "</li>")
-
-
-
-
-
-
+		$("#tweetList").append("<li class='list-group-item'><div class='Htitle'><h2 class='userName'>" + JSONdata.statuses[i].user.screen_name + "</h2><p class='UserLoc'>From: " + JSONdata.statuses[i].user.location + "</p></div><p class='tweet'>" + JSONdata.statuses[i].text + "</p><div class='footer'><h5 class='time'>Status Date: " + JSONdata.statuses[i].created_at + "</h5><h3 class='faved'>Favorite Count: " + JSONdata.statuses[i].favorite_count + "</h3></div></li>")
 	};
-	$(".tweets").html(JSONdata.statuses[4].text  +  " " + JSONdata.statuses[4].favorite_count);
-	 
-} 
+	var userList = new List('myList', options);	 
+	debugger;
+}
+
+var options = {
+  valueNames: [ 'userName', 'time' ]
+};
